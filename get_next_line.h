3ba085d288b3b9a53e2564236a3af2df2a514f57
Valentin BARON <vbaron@e3r1p6.42.fr>
2016-11-12 04:06:00 +0100

    coucou

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,19 @@
 /*   By: vbaron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 22:33:32 by vbaron            #+#    #+#             */
-/*   Updated: 2016/11/03 06:26:38 by vbaron           ###   ########.fr       */
+/*   Updated: 2016/11/12 04:05:39 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "libft/libft.h"
+
+# include <fcntl.h>
+
 # define BUFF_SIZE 8
 # define BUFF_MAX 32000
 # define MAXFILE 2048
 
 int				get_next_line(int const fd, char **line);
-char			*first_call(char *str, int fd);
 #endif
